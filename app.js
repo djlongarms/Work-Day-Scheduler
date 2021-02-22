b89721ac74a8b0284423860462ffec86b88e9ec3
Nick Palmer <ylnickpalmer@gmail.com>
2021-02-21 23:27:59 -0800

    substituted jQuery for vanilla javascript

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Sets the date at the bottom of the jumbotron
-document.getElementById('currentDay').textContent = moment().format('dddd, MMMM Do')
+$('#currentDay').text(moment().format('dddd, MMMM Do'))
 
 // Creates an array to store the time-blocks for access later
 let timeBlocks = []
@@ -8,47 +8,47 @@ let timeBlocks = []
 let currentEvents = JSON.parse(localStorage.getItem('currentEvents')) || []
 
 
-// Creates section elements for times between 9AM and 5PM
+// Creates section elements for timeblocks between 9AM and 5PM
 for(let i = 0; i <= 8; i++) {
   // Creates new section element, then adds classes and inner HTML to make the section a time-block
-  let block = document.createElement("section")
-  block.className = "row time-block"
-  block.innerHTML = `
+  let block = $('<section>')
+  block.addClass("row time-block")
+  block.html(`
     <div class="col-sm-1 hour">${((i + 8) % 12) + 1}AM</div>
     <textArea class="col-sm-10"></textArea>
     <div class="col-sm-1 saveBtn">
       <i class="fa fa-save save"></i>
     </div>
-  `
+  `)
 
   // Checks if the current time-block is in the past, present, or future and adds a class accordingly
   if(i + 9 < moment().hour()) {
-    block.classList.add('past')
+    block.addClass('past')
   } else if(i + 9 > moment().hour()) {
-    block.classList.add('future')
+    block.addClass('future')
   } else {
-    block.classList.add('present')
+    block.addClass('present')
   }
 
   // Gives textArea the proper event if one has been saved previously
   if(currentEvents[i] !== undefined) {
-    block.childNodes[3].value = currentEvents[i]
+    block.children("textArea").val(currentEvents[i])
   }
 
   // Adds block to timeBlocks array, then appends block to schedule container
-  timeBlocks.push(block)
-  document.getElementById('schedule').append(block)
+  timeBlocks.push(block[0])
+  $('#schedule').append(block)
 }
 
 // Listens for a click on the save icon in a saveBtn element
-document.addEventListener('click', event => {
+$("*").click(event => {
   // Checks to ensure we have clicked the correct icon
-  if(event.target.classList.contains('save')) {
+  if($(event.target).hasClass('save')) {
     // Gets the text and stores it in newEvent
-    let newEvent = event.target.parentElement.parentElement.childNodes[3].value
+    let newEvent = $(event.target).parent().parent().children("textArea").val()
 
     // Gets index of the currently clicked block within the timeBlocks array
-    let index = timeBlocks.indexOf(event.target.parentElement.parentElement)
+    let index = jQuery.inArray($(event.target).parent().parent()[0], timeBlocks)
 
     // Sets the correspondingly indexed element of currentEvents to have the newEvent text
     currentEvents[index] = newEvent
